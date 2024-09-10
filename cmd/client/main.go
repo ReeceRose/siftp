bf46c0fa57e5a0a118b9d5b3c6250e70f4734150
Reece Rose <reecerose18@gmail.com>
2024-09-09 20:57:26 -0400

    Move to log package

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"io/fs"
+	"log"
 	"net"
 	"os"
 
@@ -14,39 +15,50 @@ import (
 )
 
 func main() {
+	logFile, err := os.OpenFile("client.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	defer logFile.Close()
+
+	multiWriter := io.MultiWriter(os.Stdout, logFile)
+	log.SetOutput(multiWriter)
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
 	if len(os.Args) < 2 {
-		println("Usage: go run cmd/client/main.go <file_path>")
+		log.Panicln("Usage: go run cmd/client/main.go <file_path>")
 		return
 	}
 
 	filePath := os.Args[1]
-	println("Client started")
+	log.Println("Client started")
 
 	tcpServer, err := net.ResolveTCPAddr(utils.TCP, utils.SERVER_ADDRESS)
 	if err != nil {
-		println("Failed to create TCP client", err)
+		log.Println("Failed to create TCP client", err)
 		return
 	}
 
 	conn, err := net.DialTCP(utils.TCP, nil, tcpServer)
 	if err != nil {
-		println("Failed to create TCP connection", err)
+		log.Println("Failed to create TCP connection", err)
 		return
 	}
 	defer conn.Close()
 
-	println("Connected to " + utils.SERVER_ADDRESS)
+	log.Println("Connected to " + utils.SERVER_ADDRESS)
 
 	file, err := os.OpenFile(filePath, 0, fs.FileMode(os.O_RDONLY))
 	if err != nil {
-		println("Failed to open file", err)
+		log.Println("Failed to open file", err)
 		return
 	}
 	defer file.Close()
 
 	fileInfo, err := file.Stat()
 	if err != nil {
-		println("Failed to get file info", err)
+		log.Println("Failed to get file info", err)
 		return
 	}
 
@@ -55,13 +67,13 @@ func main() {
 
 	hash := sha256.New()
 	if _, err := io.Copy(hash, file); err != nil {
-		println("Failed to calculate checksum", err)
+		log.Println("Failed to calculate checksum", err)
 		return
 	}
 	checksum := fmt.Sprintf("%x", hash.Sum(nil))
 
 	if _, err := file.Seek(0, 0); err != nil {
-		println("Failed to reset file pointer", err)
+		log.Println("Failed to reset file pointer", err)
 		return
 	}
 
@@ -75,27 +87,27 @@ func main() {
 	encoder := gob.NewEncoder(writer)
 	err = encoder.Encode(header)
 	if err != nil {
-		println("Failed to encode header", err)
+		log.Println("Failed to encode header", err)
 		return
 	}
 
 	// Send File Data
 	bytesCopied, err := io.Copy(writer, file)
 	if err != nil {
-		println("Failed to copy data to writer", err)
+		log.Println("Failed to copy data to writer", err)
 		return
 	}
 
 	// Flush the writer again after file transfer
 	writer.Flush()
 
-	println("File sent successfully, bytes transferred:", bytesCopied)
+	log.Println("File sent successfully, bytes transferred:", bytesCopied)
 
 	response, err := reader.ReadString('\n')
 	if err != nil {
-		println("Failed to get response from server", err)
+		log.Println("Failed to get response from server", err)
 		return
 	}
 
-	println("Server response:", response)
+	log.Println("Server response:", response)
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,7 @@ import (
 	"encoding/gob"
 	"fmt"
 	"io"
+	"log"
 	"net"
 	"os"
 
@@ -13,19 +14,30 @@ import (
 )
 
 func main() {
+	logFile, err := os.OpenFile("server.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	defer logFile.Close()
+
+	multiWriter := io.MultiWriter(os.Stdout, logFile)
+	log.SetOutput(multiWriter)
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
 	listen, err := net.Listen(utils.TCP, utils.SERVER_ADDRESS)
 	if err != nil {
-		println("Failed to create TCP client", err)
+		log.Println("Failed to create TCP client", err)
 		return
 	}
 	defer listen.Close()
 
-	println("Server has started on PORT " + utils.SERVER_PORT)
+	log.Println("Server has started on PORT " + utils.SERVER_PORT)
 
 	for {
 		conn, err := listen.Accept()
 		if err != nil {
-			println("Failed to create accept new connection", err)
+			log.Println("Failed to create accept new connection", err)
 		}
 		go recieveFile(conn)
 	}
@@ -33,7 +45,7 @@ func main() {
 
 func recieveFile(conn net.Conn) {
 	defer conn.Close()
-	println("Received a request: " + conn.RemoteAddr().String())
+	log.Println("Received a request: " + conn.RemoteAddr().String())
 	reader := bufio.NewReader(conn)
 	writer := bufio.NewWriter(conn)
 
@@ -41,19 +53,19 @@ func recieveFile(conn net.Conn) {
 	var header utils.FileHeader
 	err := decoder.Decode(&header)
 	if err != nil {
-		println("Failed to decode header", err)
+		log.Println("Failed to decode header", err)
 		return
 	}
 
-	println("Protocol Version: %s", header.Version)
-	println("Receiving file: %s", header.FileName)
-	println("File Size: %d", header.FileSize)
-	println("Checksum: %s", header.Checksum)
+	log.Printf("Protocol Version: %s\n", header.Version)
+	log.Printf("Receiving file: %s\n", header.FileName)
+	log.Printf("File Size: %d\n", header.FileSize)
+	log.Printf("Checksum: %s\n", header.Checksum)
 
 	os.Mkdir("./uploads", os.ModePerm)
 	file, err := os.Create(fmt.Sprintf("./uploads/%s", header.FileName))
 	if err != nil {
-		println("Failed to create file to download to", err)
+		log.Println("Failed to create file to download to", err)
 		return
 	}
 	defer file.Close()
@@ -64,7 +76,7 @@ func recieveFile(conn net.Conn) {
 
 	bytesCopied, err := io.Copy(writerToFile, limitedReader)
 	if err != nil {
-		println("Failed to receive file data", err)
+		log.Println("Failed to receive file data", err)
 		return
 	}
 
@@ -72,12 +84,12 @@ func recieveFile(conn net.Conn) {
 	var serverResponse string
 	if calculatedChecksum == header.Checksum {
 		serverResponse = "File received successfully. Checksum matches.\n"
-		println("File received successfully and checksum matches. Bytes transferred:", bytesCopied)
+		log.Println("File received successfully and checksum matches. Bytes transferred:", bytesCopied)
 	} else {
 		serverResponse = "Checksum mismatch!\n"
-		println("Checksum mismatch!")
-		println("Expected:", header.Checksum)
-		println("Got:     ", calculatedChecksum)
+		log.Println("Checksum mismatch!")
+		log.Println("Expected:", header.Checksum)
+		log.Println("Got:     ", calculatedChecksum)
 	}
 
 	writer.WriteString(serverResponse)
